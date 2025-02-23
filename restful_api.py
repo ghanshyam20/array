d8f96af6c06a1f76e468d3ce2775a3eb7873aa0c
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2025-02-23 23:48:12 +0200

    this is restful api

---
+++ restful_api.py
@@ -0,0 +1,16 @@
+#how to build a restful api 
+
+# what is rest ??? 
+
+# RE=Representational
+# S=State
+#T=transfer
+
+# rest is an architectural style for designing networked applications.
+
+#GET,POST,PUT,DELETE,PATCh
+
+
+
+
+
